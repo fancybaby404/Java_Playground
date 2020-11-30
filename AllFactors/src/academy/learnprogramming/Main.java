ff100d3c467cd02b6d8f3208d4a9828f10d8dd94
fancybaby404 <macato404@gmail.com>
2020-11-30 17:10:24 +0800

    Update Main.java

--- AllFactors/src/academy/learnprogramming/Main.java
+++ AllFactors/src/academy/learnprogramming/Main.java
@@ -3,6 +3,24 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       printFactors(-1);
+    }
+
+    public static void printFactors(int number){
+       //variables
+        int factor = 0;
+
+        //logic
+        if(number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+
+        }
     }
 }
