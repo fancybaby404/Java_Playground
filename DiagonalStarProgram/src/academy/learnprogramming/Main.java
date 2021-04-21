1c753aca1edec88eb9b3d5c0f19dc9acc1f0847f
Julian Charles <53817791+fancybaby404@users.noreply.github.com>
2021-04-21 21:34:08 +0800

    idk what is this

--- DiagonalStarProgram/src/academy/learnprogramming/Main.java
+++ DiagonalStarProgram/src/academy/learnprogramming/Main.java
@@ -3,6 +3,22 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number){
+        //INVALID
+        if(number < 5){
+            System.out.println("Invalid Value");
+        } else {
+
+            //print the pattern
+            for(int i = 0; i <= number; i++){
+                for (int a = 5; a <= number; a++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
     }
 }
